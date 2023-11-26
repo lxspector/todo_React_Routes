ea81c84dd3fae514f678d0964688f41566113eab
Alex Spector <lxspector@gmail.com>
2023-11-26 13:54:05 +0100

    todoV1

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => response.json())
+      .then((data) => setTodos(data));
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
